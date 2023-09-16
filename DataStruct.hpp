1b3bc9ed43a8646d2a736ed5ff48d701ad8f79f6
Rakesh Pavan <32369780+Rakeshpavan333@users.noreply.github.com>
2023-09-16 14:21:09 -0700

    Update DataStruct.hpp

--- DataStruct.hpp
+++ DataStruct.hpp
@@ -23,14 +23,18 @@ public:
     // Constructors
     DataStruct(const std::string& pathName);
     DataStruct(cv::Mat& data);
+    DataStruct(const std::string& pathName, const Eigen::Vector3i& dim, const Eigen::Vector3i& regionSz);
 
     // Public Member Functions
     void logDebugInfo();
     void saveState();
     void loadState();
 
+    cv::Mat& getSample(); 
+
 private:
     // Private Member Functions
+    void configureRegions();
     void initializePBracketAndHasPField();
     void applyFilter(cv::Mat& data, const cv::Mat& kernel, float filter_level);
     void setPBracket();
@@ -42,9 +46,8 @@ private:
     cv::Mat volume;  // 3D tensor representing the volume (placeholder type)
     Eigen::Vector3i Dim; // Full (original) volume dimensions
     Eigen::Vector3i regionSize; // Size of each sub-volume region
-    std::vector<cv::Mat> regions; // Sub-volumes (placeholder type)
-    std::map<Mnode, PField> PBracket; // Lie Bracket for the manifold 
-    std::vector<std::vector<std::vector<bool>>> hasPField; // True if voxel (x, y, z) is contained in PBracket
+    cv::Mat sampleImage;
+    std::vector<std::unique_ptr<SubVolume>> regions; // Sub-volumes 
 };
 
 #endif // DATASTRUCT_HPP
